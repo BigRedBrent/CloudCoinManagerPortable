121bafa43b75425a4df5ccca01ff040cf16c5791
BigRedBrent <18352428+BigRedBrent@users.noreply.github.com>
2023-03-06 20:41:54 -0500

    update
    will now download and update to the latest version

--- CloudCoin Manager Portable/Scripts/update.cmd
+++ CloudCoin Manager Portable/Scripts/update.cmd
@@ -1,11 +1,51 @@
 @ECHO OFF
 TITLE CloudCoin Manager Portable %CLOUDCOINMANAGERPORTABLE_new_version% - Update
 IF "%~1" == "" EXIT
+CD /D "%~dp0"
+DEL /Q "*.tmp" > NUL 2>&1
+DEL /A:H /Q "*.tmp" > NUL 2>&1
+DEL /Q "*.zip" > NUL 2>&1
+
+ECHO. & ECHO Downloading update...
+FOR /F "tokens=*" %%G IN ('BITSADMIN /LIST 1^>NUL') DO CALL :update_remove_job "%%G"
+BITSADMIN /CANCEL "CLOUDCOINMANAGERPORTABLE_UPDATE_BITFC3UEVPCG8BJNGFRU" > NUL 2>&1
+PING github.com -n 1 -w 5000 > NUL 2>&1 || GOTO update_failed
+BITSADMIN /CREATE /DOWNLOAD "CLOUDCOINMANAGERPORTABLE_UPDATE_BITFC3UEVPCG8BJNGFRU" > NUL 2>&1
+BITSADMIN /SETMAXDOWNLOADTIME "CLOUDCOINMANAGERPORTABLE_UPDATE_BITFC3UEVPCG8BJNGFRU" 20 > NUL 2>&1
+BITSADMIN /SETNOPROGRESSTIMEOUT "CLOUDCOINMANAGERPORTABLE_UPDATE_BITFC3UEVPCG8BJNGFRU" 5 > NUL 2>&1
+BITSADMIN /SETMINRETRYDELAY "CLOUDCOINMANAGERPORTABLE_UPDATE_BITFC3UEVPCG8BJNGFRU" 0 > NUL 2>&1
+BITSADMIN /SETNOTIFYCMDLINE "CLOUDCOINMANAGERPORTABLE_UPDATE_BITFC3UEVPCG8BJNGFRU" NULL NULL > NUL 2>&1
+TITLE CloudCoin Manager Portable %CLOUDCOINMANAGERPORTABLE_new_version% - Downloading Update
+BITSADMIN /TRANSFER "CLOUDCOINMANAGERPORTABLE_UPDATE_BITFC3UEVPCG8BJNGFRU" /DOWNLOAD /DYNAMIC "https://github.com/BigRedBrent/CloudCoinManagerPortable/raw/main/CloudCoinManagerPortable.zip" "%CD%\CloudCoinManagerPortable.zip"
+TITLE CloudCoin Manager Portable %CLOUDCOINMANAGERPORTABLE_new_version% - Update
+BITSADMIN /CANCEL "CLOUDCOINMANAGERPORTABLE_UPDATE_BITFC3UEVPCG8BJNGFRU" > NUL 2>&1
+FOR /F "tokens=*" %%G IN ('BITSADMIN /LIST 1^>NUL') DO CALL :update_remove_job "%%G"
+GOTO update_skip_remove_job
+:update_remove_job
+SET CLOUDCOINMANAGERPORTABLE_update_guid=%~1
+ECHO %~1 | FIND "CLOUDCOINMANAGERPORTABLE_UPDATE_BITFC3UEVPCG8BJNGFRU" > NUL 2>&1 && BITSADMIN /CANCEL %CLOUDCOINMANAGERPORTABLE_update_guid:~0,38% > NUL 2>&1
+EXIT /B
+:update_skip_remove_job
+CLS
+IF NOT EXIST "CloudCoinManagerPortable.zip" GOTO update_failed
+
+ECHO. & ECHO Extracting update...
+RMDIR /S /Q "CloudCoin Manager Portable" > NUL 2>&1
+7za.exe e CloudCoinManagerPortable.zip -spf > NUL 2>&1 || GOTO update_failed
+IF NOT DEFINED CLOUDCOINMANAGERPORTABLE_home_dir FOR %%G IN (..) DO SET CLOUDCOINMANAGERPORTABLE_home_dir=%%~fG
+MOVE /Y "%CD%\CloudCoin Manager Portable\Scripts\*" "%CLOUDCOINMANAGERPORTABLE_home_dir%\Scripts\" > NUL 2>&1 || GOTO update_failed
+MOVE /Y "%CD%\CloudCoin Manager Portable\*" "%CLOUDCOINMANAGERPORTABLE_home_dir%\" > NUL 2>&1 || GOTO update_failed
+GOTO update_exit
+
+:update_failed
 ECHO.
-ECHO Automatic updating not implemented yet.
-ECHO.
-ECHO You may find the latest version here:
-ECHO https://github.com/BigRedBrent/CloudCoinManagerPortable
+ECHO Update failed!
 ECHO.
 PAUSE
+
+:update_exit
+RMDIR /S /Q "CloudCoin Manager Portable" > NUL 2>&1
+DEL /Q "*.tmp" > NUL 2>&1
+DEL /A:H /Q "*.tmp" > NUL 2>&1
+DEL /Q "*.zip" > NUL 2>&1
 DEL "update.tmp.cmd" & EXIT
--- CloudCoin Manager Portable/Scripts/version.cmd
+++ CloudCoin Manager Portable/Scripts/version.cmd
@@ -3,10 +3,10 @@ TITLE CloudCoin Manager Portable - Checking Version
 SET CLOUDCOINMANAGERPORTABLE_version=
 SET CLOUDCOINMANAGERPORTABLE_new_version=
 CD /D "%~dp0"
-DEL /Q "%CD%\*.tmp" > NUL 2>&1
-DEL /A:H /Q "%CD%\*.tmp" > NUL 2>&1
+DEL /Q "*.tmp" > NUL 2>&1
+DEL /A:H /Q "*.tmp" > NUL 2>&1
 
-IF EXIST "%CD%\version.txt" FOR /F "tokens=* delims=" %%G in (version.txt) DO IF NOT "%%~G" == "" CALL :set_current_version "%%~G" & GOTO skip_current_version
+IF EXIST "version.txt" FOR /F "tokens=* delims=" %%G in (version.txt) DO IF NOT "%%~G" == "" CALL :set_current_version "%%~G" & GOTO skip_current_version
 GOTO skip_current_version
 :set_current_version
 SET CLOUDCOINMANAGERPORTABLE_version=%~1
@@ -18,24 +18,23 @@ TITLE CloudCoin Manager Portable %CLOUDCOINMANAGERPORTABLE_version% - Checking V
 ECHO. & ECHO Checking for script update...
 FOR /F "tokens=*" %%G IN ('BITSADMIN /LIST 1^>NUL') DO CALL :version_remove_job "%%G"
 BITSADMIN /CANCEL "CLOUDCOINMANAGERPORTABLE_VERSION_BITFC3UEVPCG8BJNGFRU" > NUL 2>&1
-PING raw.githubusercontent.com -n 1 -w 5000 > NUL 2>&1 || GOTO version_done
+PING github.com -n 1 -w 5000 > NUL 2>&1 || GOTO version_done
 BITSADMIN /CREATE /DOWNLOAD "CLOUDCOINMANAGERPORTABLE_VERSION_BITFC3UEVPCG8BJNGFRU" > NUL 2>&1
 BITSADMIN /SETMAXDOWNLOADTIME "CLOUDCOINMANAGERPORTABLE_VERSION_BITFC3UEVPCG8BJNGFRU" 20 > NUL 2>&1
 BITSADMIN /SETNOPROGRESSTIMEOUT "CLOUDCOINMANAGERPORTABLE_VERSION_BITFC3UEVPCG8BJNGFRU" 5 > NUL 2>&1
 BITSADMIN /SETMINRETRYDELAY "CLOUDCOINMANAGERPORTABLE_VERSION_BITFC3UEVPCG8BJNGFRU" 0 > NUL 2>&1
 BITSADMIN /SETNOTIFYCMDLINE "CLOUDCOINMANAGERPORTABLE_VERSION_BITFC3UEVPCG8BJNGFRU" NULL NULL > NUL 2>&1
-BITSADMIN /TRANSFER "CLOUDCOINMANAGERPORTABLE_VERSION_BITFC3UEVPCG8BJNGFRU" /DOWNLOAD /DYNAMIC "https://raw.githubusercontent.com/BigRedBrent/CloudCoinManagerPortable/main/version.txt" "%CD%\version.tmp" > NUL 2>&1
+BITSADMIN /TRANSFER "CLOUDCOINMANAGERPORTABLE_VERSION_BITFC3UEVPCG8BJNGFRU" /DOWNLOAD /DYNAMIC "https://github.com/BigRedBrent/CloudCoinManagerPortable/raw/main/version.txt" "%CD%\version.tmp" > NUL 2>&1
 BITSADMIN /CANCEL "CLOUDCOINMANAGERPORTABLE_VERSION_BITFC3UEVPCG8BJNGFRU" > NUL 2>&1
 FOR /F "tokens=*" %%G IN ('BITSADMIN /LIST 1^>NUL') DO CALL :version_remove_job "%%G"
 GOTO version_skip_remove_job
 :version_remove_job
-ECHO %~1
 SET CLOUDCOINMANAGERPORTABLE_version_guid=%~1
 ECHO %~1 | FIND "CLOUDCOINMANAGERPORTABLE_VERSION_BITFC3UEVPCG8BJNGFRU" > NUL 2>&1 && BITSADMIN /CANCEL %CLOUDCOINMANAGERPORTABLE_version_guid:~0,38% > NUL 2>&1
 EXIT /B
 :version_skip_remove_job
 CLS
-IF NOT EXIST "%CD%\version.tmp" GOTO version_done
+IF NOT EXIST "version.tmp" GOTO version_done
 
 FOR /F "tokens=* delims=" %%G in (version.tmp) DO IF NOT "%%~G" == "" CALL :set_version "%%~G" & GOTO skip_version
 GOTO skip_version
@@ -44,8 +43,8 @@ SET CLOUDCOINMANAGERPORTABLE_new_version=%~1
 EXIT /B
 :skip_version
 
-DEL /Q "%CD%\*.tmp" > NUL 2>&1
-DEL /A:H /Q "%CD%\*.tmp" > NUL 2>&1
+DEL /Q "*.tmp" > NUL 2>&1
+DEL /A:H /Q "*.tmp" > NUL 2>&1
 
 IF "%CLOUDCOINMANAGERPORTABLE_new_version%" == "" GOTO version_done
 IF "%CLOUDCOINMANAGERPORTABLE_version%" == "%CLOUDCOINMANAGERPORTABLE_new_version%" (
@@ -65,5 +64,5 @@ COPY /Y update.cmd update.tmp.cmd || GOTO version_done
 START "" update.tmp.cmd "1" & EXIT
 
 :version_done
-DEL /Q "%CD%\*.tmp" > NUL 2>&1
-DEL /A:H /Q "%CD%\*.tmp" > NUL 2>&1
+DEL /Q "*.tmp" > NUL 2>&1
+DEL /A:H /Q "*.tmp" > NUL 2>&1
