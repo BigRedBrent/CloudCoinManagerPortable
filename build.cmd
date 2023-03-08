911d521bb53f5b8f77121a66ed892ba5214e5a36
BigRedBrent <18352428+BigRedBrent@users.noreply.github.com>
2023-03-08 13:33:47 -0500

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -5,28 +5,24 @@ CALL :main 9>>"%~f0"
 EXIT /B
 :main
 @ECHO OFF
-CLS
-ECHO. & ECHO 3 & ECHO. & PAUSE & CLS & ECHO. & ECHO 2 & ECHO. & PAUSE & CLS & ECHO. & ECHO 1 & ECHO. & PAUSE & CLS
+CLS & ECHO. & ECHO 3 & ECHO. & PAUSE & CLS & ECHO. & ECHO 2 & ECHO. & PAUSE & CLS & ECHO. & ECHO 1 & ECHO. & PAUSE & CLS
 CD /D "%~dp0"
-DEL "%CD%\version.tmp" >NUL 2>&1
-DEL "%CD%\CloudCoinManagerPortable.tmp" >NUL 2>&1
-CALL "%CD%\CloudCoin Manager Portable\Start CloudCoin Manager Portable.cmd" "1"
+DEL "version.tmp" >NUL 2>&1
+DEL "CloudCoinManagerPortable.tmp" >NUL 2>&1
+CALL "CloudCoin Manager Portable\Start CloudCoin Manager Portable.cmd" "1"
+IF "%CLOUDCOINMANAGERPORTABLE_version%" == "" GOTO build_fail
 ECHO %CLOUDCOINMANAGERPORTABLE_version%> version.tmp
 FOR /F "tokens=* delims=" %%G in (version.tmp) DO IF NOT "%%~G" == "%CLOUDCOINMANAGERPORTABLE_version%" GOTO build_fail
-MOVE /Y "%CD%\version.tmp" "%CD%\version.txt" >NUL 2>&1 || GOTO build_fail
+MOVE /Y "version.tmp" "version.txt" >NUL 2>&1 || GOTO build_fail
 "%CD%\CloudCoin Manager Portable\Scripts\7za.exe" a -tzip "%CD%\CloudCoinManagerPortable.tmp" "%CD%\CloudCoin Manager Portable\" -mx=9 || GOTO build_fail
-MOVE /Y "%CD%\CloudCoinManagerPortable.tmp" "%CD%\CloudCoinManagerPortable.zip" >NUL 2>&1 || GOTO build_fail
-ECHO.
-ECHO.
+MOVE /Y "CloudCoinManagerPortable.tmp" "CloudCoinManagerPortable.zip" >NUL 2>&1 || GOTO build_fail
+ECHO. & ECHO. & ECHO.
 ECHO Completed successfully.
-ECHO.
-PAUSE
-EXIT
+ECHO. & PAUSE & EXIT
 :build_fail
-ECHO.
-ECHO.
+ECHO. & ECHO. & ECHO.
 ECHO Failed!
 ECHO.
-DEL "%CD%\version.tmp" >NUL 2>&1
-DEL "%CD%\CloudCoinManagerPortable.tmp" >NUL 2>&1
-PAUSE
+DEL "version.tmp" >NUL 2>&1
+DEL "CloudCoinManagerPortable.tmp" >NUL 2>&1
+PAUSE & EXIT
